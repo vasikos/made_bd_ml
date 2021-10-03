1f6b9fae1ca6d47278aba21645090a3490ce13e0
Василий Гинз <v.ginz@city-mobil.ru>
2021-10-03 17:15:05 +0300

    MapReduce cleaning

--- hw1/part_3/mapper.py
+++ hw1/part_3/mapper.py
@@ -8,20 +8,10 @@ price_sum = 0
 price2_sum = 0
 price_cnt = 0
 for line in csv.reader(sys.stdin):
-    # line = line.strip('\n')
-    # print(list(csv.reader([line.strip()])))
-    # splitted = list(csv.reader([line.strip()]))
-    # print(line)
-#     if len(splitted) < 1:
-#         print(line + 'bad_split')
-#         continue
-#     # print(splitted[0], print(type(splitted[0])))
     if len(line) < 10:
-        # print(line + 'bad_len' + str(len(splitted[0])))
         continue
     
     price_raw = line[9]
-#     # print(price_raw)
 
     try:
         price = int(price_raw)
@@ -29,7 +19,6 @@ for line in csv.reader(sys.stdin):
         price2_sum += price * price
         price_cnt += 1
     except:
-        # print(line + 'bad_price_' + price_raw)
         pass
 
 print(price_cnt, price_sum, price2_sum)
